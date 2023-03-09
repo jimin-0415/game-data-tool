3a29496661563c33fc8af877d464aa95bfc7616f
jiminKim <programmer0415@gmail.com>
2023-03-10 00:24:57 +0900

    Feat: 데이터 로드되도록 수정
    - 데이터 로드 기능 추가
    - 기본 LogSheet 추가

--- game-data-tool/Program.cs
+++ game-data-tool/Program.cs
@@ -1,11 +1,17 @@
 ﻿using System;
 using System.Data;
+using System.Text;
 using ExcelDataReader;
 
 class Program
 { 
     static void Main(string[] args)
     {
+        Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+
+        DataLoader dataLoader = new ExcelDataLoader("/common/study");
+        dataLoader.Init();
+        dataLoader.Load();
         System.Console.WriteLine("Hello World !");
     }
 }
--- game-data-tool/source/ExcelDataLoader.cs
+++ game-data-tool/source/ExcelDataLoader.cs
@@ -1,19 +1,70 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
+using ExcelDataReader;
 
 class ExcelDataLoader : DataLoader
 {
+    string directoryPath;
+    Stopwatch timer;
+
+    public ExcelDataLoader(string path)
+    {
+        this.directoryPath = path;
+        this.timer = new Stopwatch();
+    }
+
     public virtual void Init()
     {
-        
+
+    }
+
+    private static List<string> GetTablenames(DataTableCollection tables)
+    {
+        var tableList = new List<string>();
+        foreach (var table in tables)
+        {
+            tableList.Add(table.ToString());
+        }
+
+        return tableList;
     }
 
     public virtual void Load()
     {
+        timer.Reset();
 
+        string filePath = "C:\\Users\\programmer\\Desktop\\git\\pl\\game-data-tool\\logSheet.xlsx";
+
+        var stream = new FileStream(filePath, FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
+
+        timer.Start();
+
+        using IExcelDataReader reader = ExcelReaderFactory.CreateReader(stream);
+        var openTining = timer.ElapsedMilliseconds;
+
+        DataSet dataSet;
+        // reader.IsFirstRowAsColumnNames = firstRowNamesCheckBox.Checked;
+        dataSet = reader.AsDataSet(new ExcelDataSetConfiguration()
+        {
+            UseColumnDataType = false,
+        });
+
+        List<string> talbeNames = GetTablenames(dataSet.Tables);
+
+
+
+
+
+
+        /*        if (tablenames.Count > 0)
+                    sheetCombo.SelectedIndex = 0;*/
+
+        timer.Stop();
     }
 }
-
